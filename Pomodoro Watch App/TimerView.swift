f48fa803799306c1519940756f2cfdb4323e49a8
Jye Harry <jye.harry@nine.com.au>
2024-08-12 17:24:13 +1000

    Add restart timer

--- Pomodoro Watch App/TimerView.swift
+++ Pomodoro Watch App/TimerView.swift
@@ -8,22 +8,27 @@
 import SwiftUI
 
 struct TimerView: View {
-    @State private var secsRemaining = 5
+    static let startingTime = 60 * 25
+    @State private var secsRemaining = startingTime
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        VStack {
-            Text("Time: \(formatTime(secsRemaining))")
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.white)
-                .padding(.horizontal, 20)
-                .padding(.vertical, 5)
-                .background(.blue.opacity(0.75))
-                .clipShape(.capsule)
-        }
-        .onReceive(timer) { _ in
-            if secsRemaining > 0 {
-                secsRemaining -= 1
+        ScrollView {
+            VStack {
+                Text("Time: \(formatTime(secsRemaining))")
+                    .multilineTextAlignment(.center)
+                    .font(.title2)
+
+                Button(action: restartTime) {
+                    Text("Restart timer")
+                        .foregroundStyle(.white)
+                }
+            }
+            .onReceive(timer) { _ in
+                if secsRemaining > 0 {
+                    secsRemaining -= 1
+                    return
+                }
             }
         }
     }
@@ -34,6 +39,10 @@ struct TimerView: View {
         formatter.unitsStyle = .positional
         formatter.zeroFormattingBehavior = .pad
         return formatter.string(from: TimeInterval(value)) ?? "00:00"
+    }
+    
+    func restartTime() {
+        secsRemaining = TimerView.startingTime
     }
 }
 
