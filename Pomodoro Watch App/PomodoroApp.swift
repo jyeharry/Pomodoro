d7cca6f57a1cee88fd95d23f74ca4b1164852884
Jye Harry <jye.harry@nine.com.au>
2024-08-12 15:17:17 +1000

    Initial Commit

---
+++ Pomodoro Watch App/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro Watch App
+//
+//  Created by Jye Harry on 12/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Jye Harry on 12/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
